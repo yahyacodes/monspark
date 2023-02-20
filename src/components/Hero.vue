e7e71ea6b373ded7ab578d16a61247b44952dabb
yahyacodes <yahya.codin@gmail.com>
2023-02-20 23:37:07 +0300

    m

---
+++ src/components/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="hero-bg py-64">
+    <div>
+      <Navbar class="mt-4" />
+    </div>
+
+    <div class="container text-center">
+      <h1 class="header-tag text-white leading-none -mt-20">
+        Focus on your app, <br />
+        leave monitoring to <br />
+        <span class="text-purple-500">Monspark</span>
+      </h1>
+      <img
+        src="../assets/monspark-users-croped.png"
+        alt=""
+        class="w-40 mx-auto mt-10"
+      />
+      <p class="text-white mx-auto font-semibold text-base mt-4">
+        Loved by teams from 110+ countries
+      </p>
+      <button
+        type="button"
+        class="text-white bg-blue-700 font-medium rounded-full text-sm px-5 py-2 text-center mx-auto mt-10"
+      >
+        Sign up for free
+      </button>
+      <p class="text-gray-400 font-semibold mx-auto text-xs mt-4">
+        OR SIGN IN WITH:
+      </p>
+      <img
+        src="../assets/google-icon.svg"
+        alt=""
+        class="w-8 h-8 mx-auto mt-4"
+      />
+    </div>
+  </section>
+</template>
+<script>
+import Navbar from "./Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0">
+    <div
+      class="container-nav flex flex-wrap items-center justify-between mx-auto"
+    >
+      <span
+        class="self-center text-xl font-semibold whitespace-nowrap text-white"
+        >MonSpark</span
+      >
+      <div class="flex md:order-2">
+        <button
+          type="button"
+          class="text-white bg-blue-700 font-medium rounded-full text-sm px-5 py-2 text-center mr-3 md:mr-0"
+        >
+          Login
+        </button>
+        <button
+          data-collapse-toggle="navbar-sticky"
+          type="button"
+          class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100"
+          aria-controls="navbar-sticky"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div
+        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        id="navbar-sticky"
+      >
+        <ul
+          class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 pl-3 pr-4 text-white md:p-0"
+              aria-current="page"
+              >Features</a
+            >
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0">About</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0"
+              >Pricing</a
+            >
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0"
+              >Integrations</a
+            >
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0">Blogs</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0"
+              >Company</a
+            >
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0"
+              >Contact</a
+            >
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0"
+              >Community</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
